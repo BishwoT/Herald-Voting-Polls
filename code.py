4928c4d126ab340d6fc0310d0126595cb6e703be
manishmanandhar10 <np03cs4s240133@heraldcollege.edu.np>
2025-04-24 23:32:40 +0545

    Add files via upload

---
+++ code.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+def verify_code_view(request):
+    if request.method == 'POST':
+        user_input_code = request.POST.get('code')
+        stored_code = request.session.get('verification_code')  
+
+        if user_input_code == stored_code:
+            messages.success(request, 'Verification successful!')
+            return redirect('signup')  # or wherever you want to take them next
+        else:
+            messages.error(request, 'Invalid verification code. Try again.')
+
+    return render(request, 'verify_code.html')
